d53d08311d1567560652d08ad66f9804092652d7
swagjjs <liazemcova00@gmail.com>
2025-06-08 21:10:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,40 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
+      }
+    }
+  }
+  return count;
+}
+
+int countPairs2(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int complement = value - arr[i];
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == complement) {
+        count++;
+        int left_ptr = mid - 1;
+        while (left_ptr >= left && arr[left_ptr] == complement) {
+          count++;
+          left_ptr--;
+        }
+        int right_ptr = mid + 1;
+        while (right_ptr <= right && arr[right_ptr] == complement) {
+          count++;
+          right_ptr++;
+        }
+        break;
+      } else if (arr[mid] < complement) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
   }
@@ -27,55 +57,23 @@ int countPairs3(int *arr, int len, int value) {
       int right_val = arr[right];
       int left_count = 0;
       int right_count = 0;
-      while (left < len && arr[left] == left_val) {
+      while (arr[left] == left_val) {
         left++;
         left_count++;
       }
-      while (right >= 0 && arr[right] == right_val) {
+      while (arr[right] == right_val) {
         right--;
         right_count++;
       }
       count += left_count * right_count;
-    } else if (sum < value) {
+      }else if (sum < value) {
       left++;
-    } else {
+      } else {
       right--;
     }
   }
   return count;
 }
 
-int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int complement = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-        
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == complement) {
-        count++;
-        int left_ptr = mid - 1;
-        while (left_ptr >= left && arr[left_ptr] == complement) {
-          count++;
-          left_ptr--;
-        }
-        int right_ptr = mid + 1;
-        while (right_ptr <= right && arr[right_ptr] == complement) {
-          count++;
-          right_ptr++;
-        }
-        break;
-      }
-      else if (arr[mid] < complement) {
-        left = mid + 1;
-      }
-      else {
-        right = mid - 1;
-      }
-    }
-  }
-  return count;
-}
+
 
